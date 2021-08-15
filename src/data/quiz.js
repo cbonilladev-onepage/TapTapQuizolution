2f9a74227a5560e931503ddefe16bae27605b6a5
cabonilla <christian.bonilla01@gmail.com>
2021-08-14 18:26:40 -0700

    Initial commit.

---
+++ src/data/quiz.js
@@ -0,0 +1,148 @@
+import q1 from '../assets/q1.png'
+import q10 from '../assets/q10.png'
+import q2 from '../assets/q2.png'
+import q3 from '../assets/q3.png'
+import q4 from '../assets/q4.png'
+import q5 from '../assets/q5.png'
+import q6 from '../assets/q6.png'
+import q7 from '../assets/q7.png'
+import q8 from '../assets/q8.png'
+import q9 from '../assets/q9.png'
+
+const question = {
+	"response_code": 0,
+	"results": [
+		{
+			"category": "Python",
+			"type": "multiple",
+			"difficulty": "hard",
+			"question": "What is the output of the following code?",
+			"image": q1,
+			"correct_answer": "30",
+			"incorrect_answers": [
+				"UNDEFINED",
+				"20",
+				"40"
+			]
+		},
+		{
+			"category": "Python",
+			"type": "multiple",
+			"difficulty": "hard",
+			"question": "What is the output of the following code?",
+			"image": q2,
+			"correct_answer": "yn",
+			"incorrect_answers": [
+				"py",
+				"na",
+				"pn"
+			]
+		},
+		{
+			"category": "Python",
+			"type": "multiple",
+			"difficulty": "hard",
+			"question": "What is the output of the following code?",
+			"image": q3,
+			"correct_answer": "True & False",
+			"incorrect_answers": [
+				"True & True",
+				"False & True",
+				"False & False"
+			]
+		},
+		{
+			"category": "Python",
+			"type": "multiple",
+			"difficulty": "hard",
+			"question": "What is the output of the following code?",
+			"image": q4,
+			"correct_answer": "10, 11, 12, 13, 14",
+			"incorrect_answers": [
+				"11, 12, 13, 14, 15",
+				"10, 9, 8, 7, 6",
+				"10, 12, 14, 16, 18"
+			]
+		},
+		{
+			"category": "Python",
+			"type": "multiple",
+			"difficulty": "hard",
+			"question": "What is the output of the following code?",
+			"image": q5,
+			"correct_answer": "25, 125",
+			"incorrect_answers": [
+				"25, 50",
+				"20, 140",
+				"40, 400"
+			]
+		},
+		{
+			"category": "Python",
+			"type": "multiple",
+			"difficulty": "hard",
+			"question": "What is the output of the following code?",
+			"image": q6,
+			"correct_answer": "['Jon', 'Kelly', 'Scott', 'Jesse']",
+			"incorrect_answers": [
+				"['Jon', 'Scott', 'Kelly', 'Jesse']",
+				"['Jesse', 'Kelly', 'Scott', 'Jon']",
+				"['Scott', 'Kelly', 'Jon', 'Jesse']"
+			]
+		},
+		{
+			"category": "Python",
+			"type": "multiple",
+			"difficulty": "hard",
+			"question": "What is the output of the following code?",
+			"image": q7,
+			"correct_answer": "Error. Mixing operators between numbers and strings are not supported!",
+			"incorrect_answers": [
+				"6",
+				"123",
+				"101"
+			]
+		},
+		{
+			"category": "Python",
+			"type": "multiple",
+			"difficulty": "hard",
+			"question": "What is the output of the following code?",
+			"image": q8,
+			"correct_answer": "maJ",
+			"incorrect_answers": [
+				"mes",
+				"Bon",
+				"ema"
+			]
+		},
+		{
+			"category": "Python",
+			"type": "multiple",
+			"difficulty": "hard",
+			"question": "What is the output of the following code?",
+			"image": q9,
+			"correct_answer": "Salary: 12000 & Salary: 8000",
+			"incorrect_answers": [
+				"Salary: 8000 & Salary: 12000",
+				"Salary: 8000",
+				"Salary: 12000"
+			]
+		},
+		{
+			"category": "Python",
+			"type": "multiple",
+			"difficulty": "hard",
+			"question": "What is the output of the following code?",
+			"image": q10,
+			"correct_answer": "JamesJamesJamesJamesJamesJames",
+			"incorrect_answers": [
+				"JamesJamesJamesJamesJames",
+				"James*2*3",
+				"JamesJamesJames"
+			]
+		}
+	]
+}
+
+export default question
